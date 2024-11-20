ca3a3049399c61ceb8d22c79fb6fa8feec9c75d0
Rao Arsalan <rao.arsalan@carnivalist.com>
2024-11-20 12:33:45 +0500

    query message changed

--- src/queries/queries.controller.ts
+++ src/queries/queries.controller.ts
@@ -54,7 +54,7 @@ export class QueriesController {
     @ApiBody({ type: CreateQueryDto })
     async insert(@Body() body: CreateQueryDto, @Req() req: Request) {
         const createOption = await this.quiriesService.insertOption(body);
-        return { message: 'Your message have been sent successfully', data: createOption };
+        return { message: 'Your message has been sent successfully', data: createOption };
     }
 
     @ApiBearerAuth(AuthorizationHeader)
