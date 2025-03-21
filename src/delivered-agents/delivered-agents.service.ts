3f30d951f1ffcf58de59c48aac64143b1a803b47
Rao Arsalan <rao.arsalan@carnivalist.com>
2025-03-22 02:05:46 +0500

    issue

--- src/delivered-agents/delivered-agents.service.ts
+++ src/delivered-agents/delivered-agents.service.ts
@@ -116,6 +116,7 @@ export class DeliveredAgentsService {
       .findOne(filter)
       .populate('agent_id')
       .populate('agent_request_id')
+      .populate('company_id')
       .populate('created_by', 'first_name last_name image roles')
       .populate('updated_by', 'first_name last_name image roles')
       .populate('company_owner_id', 'first_name last_name image roles');
@@ -152,6 +153,7 @@ export class DeliveredAgentsService {
     )
     .populate('agent_id')
     .populate('agent_request_id')
+    .populate('company_id')
     .populate('created_by', 'first_name last_name image roles')
     .populate('updated_by', 'first_name last_name image roles')
     .populate('company_owner_id', 'first_name last_name image roles');
