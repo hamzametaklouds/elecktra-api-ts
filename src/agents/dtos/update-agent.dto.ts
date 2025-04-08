d8ae435cad5ac0de70390958fcb9b89369d79e88
Rao Arsalan <rao.arsalan@carnivalist.com>
2025-04-09 00:36:26 +0500

    aggregation changes everywhere

--- src/agents/dtos/update-agent.dto.ts
+++ src/agents/dtos/update-agent.dto.ts
@@ -1,8 +1,51 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateAgentDto } from './create-agent.dto';
-import { IsBoolean, IsOptional, IsEnum } from 'class-validator';
+import { IsBoolean, IsOptional, IsEnum, IsArray, ValidateNested, IsMongoId, IsNumber, IsString } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 import { AgentStatus } from '../agents.schema';
+import { ObjectId } from 'mongoose';
+import { Type } from 'class-transformer';
+
+
+class WorkflowDto {
+
+  @ApiProperty()
+  @IsMongoId()
+  @IsOptional()
+  _id?: ObjectId;
+
+
+  @ApiProperty()
+  @IsString()
+  title: string;
+
+  @ApiProperty()
+  @IsString()
+  description: string;
+
+  @ApiProperty()
+  @IsNumber()
+  price: number;
+
+  @ApiProperty()
+  @IsBoolean()
+  @IsOptional()
+  is_disabled?: boolean;
+
+  @ApiProperty()
+  @IsArray()
+  @IsMongoId({ each: true })
+  @IsOptional()
+  integrations?: ObjectId[];
+
+  @ApiProperty()
+  @IsNumber()
+  weeks: number;
+
+  @ApiProperty()
+  @IsNumber()
+  installation_price: number;
+}
 
 export class UpdateAgentDto extends PartialType(CreateAgentDto) {
   @ApiProperty({
@@ -13,6 +56,13 @@ export class UpdateAgentDto extends PartialType(CreateAgentDto) {
   @IsEnum(AgentStatus)
   @IsOptional()
   status?: AgentStatus;
+
+  @ApiProperty({ type: [WorkflowDto] })
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => WorkflowDto)
+  @IsOptional()
+  work_flows?: WorkflowDto[];
 
   @ApiProperty()
   @IsBoolean()
